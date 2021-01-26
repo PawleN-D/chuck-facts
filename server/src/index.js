055c5d314d1bf1fd5b9a5e1469291fe79746b298
PawleN-D <sirpawle@gmail.com>
2021-01-26 18:18:43 +0200

    reset and adjusted code to function locally

--- server/src/index.js
+++ server/src/index.js
@@ -1,22 +1,20 @@
-const { ApolloServer } = require('apollo-server');
-const typeDefs = require('./schema');
+const { ApolloServer } = require("apollo-server");
+const typeDefs = require("./schema");
 
-const JokesAPI = require('./datasource');
-const resolvers = require('./resolvers');
+const JokesAPI = require("./datasource");
+const resolvers = require("./resolvers");
 
 const server = new ApolloServer({
-    typeDefs,
-    resolvers,
-    dataSources: () => ({
-      jokesAPI: new JokesAPI(),
-
-    })
-  });
+  typeDefs,
+  resolvers,
+  dataSources: () => ({
+    jokesAPI: new JokesAPI(),
+  }),
+});
 
 server.listen().then(() => {
-    console.log(`
+  console.log(`
       Server is running!
       Listening on port 4000!
     `);
-  });
-  
+});
